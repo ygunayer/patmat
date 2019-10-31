01962d4a253b0623f9495660035c1ef740a00470
Yalin Gunayer <yalin.gunayer@gmail.com>
2019-10-31 15:21:05 +0300

    Also export types

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import {Sync} from './sync';
 import {Async} from './async';
 
-import {Sync as SyncData, Async as AsyncData, MatchBuilder, SyncMatcher, AsyncMatcher, SyncCase, AsyncCase} from './types';
+import * as Types from './types';
 
 export const _ = Symbol();
 
-class PatMat implements MatchBuilder {
-  match<In, Out>(value: SyncData<In>): SyncMatcher<In, Out>;
-  match<In, Out>(value: PromiseLike<In>): AsyncMatcher<In, Out>;
-  match<In, Out>(value: any): SyncMatcher<In, Out>|AsyncMatcher<In, Out> {
+class PatMat implements Types.MatchBuilder {
+  match<In, Out>(value: Types.Sync<In>): Types.SyncMatcher<In, Out>;
+  match<In, Out>(value: PromiseLike<In>): Types.AsyncMatcher<In, Out>;
+  match<In, Out>(value: any): Types.SyncMatcher<In, Out>|Types.AsyncMatcher<In, Out> {
     if (typeof value.then === 'function') {
       return Async.match<In, Out>(value);
     }
@@ -18,5 +18,6 @@ class PatMat implements MatchBuilder {
 }
 
 const patmat = new PatMat();
+
 export const match = patmat.match;
-export {Sync, Async};
+export {Types, Sync, Async};
